804902a88f1df11d80c6ad019f165a4984532c44
alan825444 <88468103+alan825444@users.noreply.github.com>
2022-06-14 18:18:49 +0800

    Sasd

--- modules/mysql_config.js
+++ modules/mysql_config.js
@@ -2,10 +2,10 @@ require('dotenv').config();
 const mysql = require('mysql2');
 
 const pool = mysql.createPool({
-    host:us-cdbr-east-05.cleardb.net,
-    user:b1b529bee2e41d,
-    password:735fced4,
-    database:heroku_5472fd0cc8e83fb,
+    host:'us-cdbr-east-05.cleardb.net',
+    user:'b1b529bee2e41d',
+    password:'735fced4',
+    database:'heroku_5472fd0cc8e83fb',
     connectionLimit:10,
     waitForConnections:true,
     dateStrings: true
