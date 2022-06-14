181511d81560708705c42009826840d905836029
alan825444 <88468103+alan825444@users.noreply.github.com>
2022-06-14 18:17:27 +0800

    asd

--- modules/mysql_config.js
+++ modules/mysql_config.js
@@ -2,10 +2,10 @@ require('dotenv').config();
 const mysql = require('mysql2');
 
 const pool = mysql.createPool({
-    host:process.env.MYSQL_HOST,
-    user:process.env.MYSQL_USER,
-    password:process.env.MYSQL_PASSWORD,
-    database:process.env.MYSQL_DB,
+    host:us-cdbr-east-05.cleardb.net,
+    user:b1b529bee2e41d,
+    password:735fced4,
+    database:heroku_5472fd0cc8e83fb,
     connectionLimit:10,
     waitForConnections:true,
     dateStrings: true
