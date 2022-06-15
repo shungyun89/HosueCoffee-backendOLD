0ac221d366863bf190658d18a117a56b4d980be9
shungyun89 <27332358asdf@gmail.com>
2022-06-16 03:12:46 +0800

    aaa

--- controllers/get_controller.js
+++ controllers/get_controller.js
@@ -10,7 +10,7 @@ module.exports = class GetPayment {
     // const totalPrice = Number(fetchData);
 
     let base_param = {
-      MerchantTradeNo: payUid, //請帶20碼uid, ex: f0a0d7e9fae1bb72bc93
+      MerchantTradeNo: "f0a0d7e9fae1bb72bc93", //請帶20碼uid, ex: f0a0d7e9fae1bb72bc93
       MerchantTradeDate: onTimeValue(), //ex: 2017/02/13 15:45:30
       TotalAmount: '100',
       TradeDesc: '測試交易描述',
