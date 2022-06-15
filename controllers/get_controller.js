2fb1f93efea742c478abab74e9ed7ec113289a75
shungyun89 <27332358asdf@gmail.com>
2022-06-16 02:53:50 +0800

    aa

--- controllers/get_controller.js
+++ controllers/get_controller.js
@@ -1,17 +1,17 @@
 module.exports = class GetPayment {
 
-//   payUid(req, res) {
-//     let uid = randomValue(10, 99) + "1234567890234567" + randomValue(10, 99);
-//     res.render('payment', { uid: uid  });
-//   }
+  payUid(req, res) {
+    let uid = randomValue(10, 99) + "1234567890234567" + randomValue(10, 99);
+    res.render('payment', { uid: uid  });
+  }
   
   payAction(req,res,next){
     const uid = req.query.uid;
-    const totalPrice = req.query.totalPrice;
+    const totalPrice = Number(fetchData);
 
     let base_param = {
-      MerchantTradeNo: f0a0d7e9fae1bb72bc93, //請帶20碼uid, ex: f0a0d7e9fae1bb72bc93
-      MerchantTradeDate: '2017/02/13 15:45:30', //ex: 2017/02/13 15:45:30
+      MerchantTradeNo: uid, //請帶20碼uid, ex: f0a0d7e9fae1bb72bc93
+      MerchantTradeDate: onTimeValue(), //ex: 2017/02/13 15:45:30
       TotalAmount: totalPrice,
       TradeDesc: '測試交易描述',
       ItemName: 'HouseCoffe網路購物',
